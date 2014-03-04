beb280445ba4db1ceff28ec7fd59db0d77f3f418
Yusuke Sakamoto <yus.sakamoto@gmail.com>
2014-03-04 17:33:05 -0600

    seems like working. velocity is set to zero to solve for the displacement. if the pressure is zero too, then it should give the same result as hyperelastic problem.

--- vic-main.py
+++ vic-main.py
@@ -17,7 +17,7 @@ ffc_options = {"optimize": True, \
                "precompute_ip_const": True}
 
 # Create mesh and define function space
-m_num = 10 # number of mesh in each dim
+m_num = 6 # number of mesh in each dim
 mesh = UnitCubeMesh(m_num, m_num, m_num)
 d = u.geometric_dimension() # dimension
 
@@ -25,19 +25,19 @@ d = u.geometric_dimension() # dimension
 porder = 2
 Pu = VectorFunctionSpace(mesh, "Lagrange", porder) # space for displacements
 Pp = FunctionSpace(mesh, "Lagrange", porder-1)     # space for pressure
-V = MixedFunctionSpace([Pp,Pu])                    # mixed space
+V = MixedFunctionSpace([Pu,Pp])                    # mixed space
 
 # Define functions
 dup = TrialFunction(V)    # Incremental displacement-pressure
-vq  = TestFunction(V)     # Test function
+wq  = TestFunction(V)     # Test function
 up  = Function(V)         # Displacement-pressure from previous iteration
-dupsol = Function(V)      # Function in the mixed space to store the solution of the linearized problem
-p, u = split(up)          # Function in each subspace to write the functional
+u, p = split(up)          # Function in each subspace to write the functional
+w, q = split(wq)          # Test Function split
 
 # Loads
 B  = Constant((0.0,  0.0, 0.0))  # Body force per unit volume
 T  = Constant((0.0,  0.0, 0.0))  # Traction force on the boundary
-
+g_bar = Constant(0.0)            # Normal flux
 
 # Mark boundary subdomians
 top    = CompiledSubDomain("near(x[2], side) && on_boundary", side = 1.0)
@@ -46,18 +46,20 @@ bottom = CompiledSubDomain("near(x[2], side) && on_boundary", side = 0.0)
 # Define Dirichlet boundary (x = 0 or x = 1)
 d_top    = Expression(("0.0", "0.0", "0.5"))
 d_bottom = Expression(("0.0", "0.0", "0.0"))
-bc_top    = DirichletBC(V, d_top, top)
-bc_bottom = DirichletBC(V, d_bottom, bottom)
+bc_top    = DirichletBC(V.sub(0), d_top, top)
+bc_bottom = DirichletBC(V.sub(0), d_bottom, bottom)
 bcs = [bc_top, bc_bottom]
+## can predcribe dirichlet pressure condition
 
 # Kinematics
 I = Identity(d)             # Identity tensor
 F = I + grad(u)             # Deformation gradient - seems like underyling coordinates is initial
-F = variable(F)
+F = variable(F)             # Make F a variable for tensor differentiations
 C = F.T*F                   # Right Cauchy-Green tensor
+invF = inv(F)
 
 # virtual Kinematics
-ddotF = grad(v)
+ddotF = grad(w)
 ddotE = 1.0/2.0 * (ddotF.T*F + F.T*ddotF)
 
 # Invariants of deformation tensors
@@ -69,37 +71,39 @@ IIIc = det(C)
 Ee, nu = 10.0, 0.45
 mu, lmbda = Constant(Ee/(2*(1 + nu))), Constant(Ee*nu/((1 + nu)*(1 - 2*nu)))
 
+# Permeability
+perm = 0.1
+K_perm = perm*I
+
 # Strain energy density (compressible neo-Hookean model)
 # psi = (mu/2.0)*(Ic - 3) - mu*ln(J) + (lmbda/2.0)*(ln(J))**2
-# psi = lmbda/2*(tr(E)**2) + mu*tr(E*E)
 
 # Strain energy density (nearly incompressible neo-hookean model)
 kappa = mu*10
 C_hat = (IIIc)**(-1.0/3.0)*C
 psi = mu/2.0*(tr(C_hat)-3) + 1.0/2.0*kappa*ln(J)**2.0
-# psi = mu/2*(tr(C_hat)-3) + kappa/2*(J-1)**2
 
 # PK1 stress tensor
 P = diff(psi,F)
 # PK2 stress tensor
 S = inv(F)*P
 
-# PK2 stress tensor (need to make C or E as variables)
-# S = 2*diff(psi, C)
-# S = diff(psi,E)
-# S = mu*(I-invC)+lmbda*ln(J)*invC
-
 # Total potential energy
 Pi = psi*dx - dot(B, u)*dx - dot(T, u)*ds
 
-# Compute residual
-R = derivative(Pi, u, v)
+# Compute residual (no velocity for now)
+R = (inner(S, ddotE) + dot(J*(K_perm*invF.T*grad(p)), invF.T*grad(q)))*dx \
+    - p*J*inner(ddotF, invF.T)*dx  \
+    - (inner(T,w))*ds + (inner(g_bar,q))*ds 
+# R = (inner(S, ddotE) + dot(J*(K_perm*invF.T*grad(p)), invF.T*grad(q)))*dx \
+#     - p*J*inner(ddotF, invF.T)*dx + (q*J*inner(grad(v),invF.T))*dx \
+#     - (inner(T,w))*ds + (inner(g_bar,q))*ds 
 
 # Compute Jacobian of F
-Jac = derivative(R, u, du)
+Jac = derivative(R, up, dup)
 
 set_log_level(DEBUG)
-problem = NonlinearVariationalProblem(R, u, bcs=bcs, J=Jac)
+problem = NonlinearVariationalProblem(R, up, bcs=bcs, J=Jac)
 solver = NonlinearVariationalSolver(problem)
 solver.parameters["newton_solver"]["linear_solver"] = "bicgstab"
 solver.parameters["newton_solver"]["preconditioner"] = "ilu"
@@ -107,7 +111,8 @@ solver.solve()
 
 # Save solution in VTK format
 file = File("displacement.pvd");
-file << u;
+file << up.sub(0);
 
 # Plot and hold solution
 plot(u, mode = "displacement", axes=True, interactive = True)
+plot(p, title="pressure", axes=True, interactive = True)
