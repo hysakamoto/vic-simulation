255829ad13390529c89c39370e9f869595029eff
Yusuke Sakamoto <yus.sakamoto@gmail.com>
2014-03-10 22:52:19 -0500

    omega addex

--- vic_main.py
+++ vic_main.py
@@ -6,11 +6,11 @@ from dolfin import *
 from vic_func import *
 import pdb
 
-reload(vic_func)
 
 # Begin simulation
 m_num = 10
 p_order = 1
 dt     = 0.2       # time step
+omega = 0.5
 
-vic_sim( m_num, p_order, dt )
+vic_sim( m_num, p_order, dt, omega )
