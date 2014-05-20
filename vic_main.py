1e92daf28afa2a7baa4692175128369808d4bacc
Yusuke Sakamoto <yus.sakamoto@gmail.com>
2014-05-20 16:58:49 -0500

    nonlinearity of the boundary term not considered?

--- vic_main.py
+++ vic_main.py
@@ -42,11 +42,11 @@ errors_u = []
 errors_p = []
 u_maxmax = []
 
-# sim_name = 'result/'
-# u_max, Eus, Eps = vic_func.vic_sim( sim_name, 
-#                                     m_num, p_order, dt, T_total, max_it, omega,
-#                                     Ee, nu, gamma, tau, perm,
-#                                     top_trac, body_force )
+sim_name = 'result/'
+u_max, Eus, Eps = vic_func.vic_sim( sim_name, 
+                                    m_num, p_order, dt, T_total, max_it, omega,
+                                    Ee, nu, gamma, tau, perm,
+                                    top_trac, body_force )
 
 # m_nums = [1,2,4,8,16]
 # sim_basename = 'mesh/'
@@ -62,40 +62,40 @@ u_maxmax = []
 #     u_maxmax.append(max(u_max))
 
 
-T_total = 10.0
-max_its = [1,2,4,8,16,32,64]
-m_num = 8
-sim_basename = 'time_cn/'
-for i in range(len(max_its)):
-    max_it = max_its[i]
-    dt = T_total/float(max_it)
-    sim_name = sim_basename + str(max_it)
-    u_max, Eus, Eps = vic_func.vic_sim( sim_name,
-                                        m_num, p_order, dt, T_total, max_it, omega,
-                                        Ee, nu, gamma, tau, perm,
-                                        top_trac, body_force )
-    errors_u.append(sum(Eus)*dt)
-    errors_p.append(sum(Eps)*dt)
-    u_maxmax.append(max(u_max))
+# T_total = 10.0
+# max_its = [1,2,4,8,16,32,64]
+# m_num = 8
+# sim_basename = 'time_cn/'
+# for i in range(len(max_its)):
+#     max_it = max_its[i]
+#     dt = T_total/float(max_it)
+#     sim_name = sim_basename + str(max_it)
+#     u_max, Eus, Eps = vic_func.vic_sim( sim_name,
+#                                         m_num, p_order, dt, T_total, max_it, omega,
+#                                         Ee, nu, gamma, tau, perm,
+#                                         top_trac, body_force )
+#     errors_u.append(sum(Eus)*dt)
+#     errors_p.append(sum(Eps)*dt)
+#     u_maxmax.append(max(u_max))
 
-print errors_u
-print errors_p
-# print u_maxmax
+# print errors_u
+# print errors_p
+# # print u_maxmax
 
-plt.loglog(max_its, errors_u,'-o'); 
-plt.show()
+# plt.loglog(max_its, errors_u,'-o'); 
+# plt.show()
 
-print 'u-convergence rate: '+ str((np.log(errors_u[0])-np.log(errors_u[-1]))/(np.log(m_nums[0])-np.log(m_nums[-1])))
-print 'p-convergence rate: '+ str((np.log(errors_p[0])-np.log(errors_p[-1]))/(np.log(m_nums[0])-np.log(m_nums[-1])))
+# print 'u-convergence rate: '+ str((np.log(errors_u[0])-np.log(errors_u[-1]))/(np.log(m_nums[0])-np.log(m_nums[-1])))
+# print 'p-convergence rate: '+ str((np.log(errors_p[0])-np.log(errors_p[-1]))/(np.log(m_nums[0])-np.log(m_nums[-1])))
 
 
-## Output convergence result
-with open(sim_basename+'conv.txt', 'w') as f:
-    f.write(str(errors_u))
-    f.write(str(errors_p))
+# ## Output convergence result
+# with open(sim_basename+'conv.txt', 'w') as f:
+#     f.write(str(errors_u))
+#     f.write(str(errors_p))
 
-    f.write('u-convergence rate: '+ str((np.log(errors_u[0])-np.log(errors_u[-1]))/(np.log(m_nums[0])-np.log(m_nums[-1]))))
-    f.write('p-convergence rate: '+ str((np.log(errors_p[0])-np.log(errors_p[-1]))/(np.log(m_nums[0])-np.log(m_nums[-1]))))
+#     f.write('u-convergence rate: '+ str((np.log(errors_u[0])-np.log(errors_u[-1]))/(np.log(m_nums[0])-np.log(m_nums[-1]))))
+#     f.write('p-convergence rate: '+ str((np.log(errors_p[0])-np.log(errors_p[-1]))/(np.log(m_nums[0])-np.log(m_nums[-1]))))
 
 
 
