4084da3e51cc4da73e5f5bb616e03840d178bcf8
Yusuke Sakamoto <yus.sakamoto@gmail.com>
2014-05-31 23:10:03 -0500

    now running simulation is more straightforward

---
+++ run_simulation.py
@@ -0,0 +1,67 @@
+""" Run a Simulation of VIC"""
+
+# Simulation of hyperelastic solid in initial configuration
+
+from vic_func import vic_sim
+import time
+
+
+def write_params(sim_name, mat_params, sim_params):
+    import os
+    try: 
+        os.makedirs(sim_name)
+    except OSError:
+        print "Could not create a directory"
+        return -1
+
+    try:
+        f = open(sim_name+'/params.txt', 'w')
+    except IOError:
+        print "Could not open file!"
+        return -1
+
+    with f:
+        f.write('mat_params = ' + str(mat_params) + '\n')
+        f.write('sim_params = ' + str(sim_params) + '\n')
+        f.close()
+
+    return 0
+
+
+def runsim( base_name, mat_params, sim_params ):
+
+    sim_name = base_name + str(sim_params['m_num']) \
+               + '-' + str(sim_params['max_it']) + '/'
+
+    exist = write_params(sim_name, mat_params, sim_params)
+
+    # if the parameter file is already created, don't run the simulation
+    if exist == -1:
+        return 0
+
+    ## starting time
+    time_start = time.time()
+
+    dt = sim_params['T_total']/float(sim_params['max_it'])
+    vic_sim( sim_name, 
+             sim_params['m_num'], 
+             sim_params['p_order'], 
+             dt, 
+             sim_params['T_total'], 
+             sim_params['max_it'], 
+             sim_params['omega'],
+             mat_params['Ee'], 
+             mat_params['nu'], 
+             mat_params['gamma'], 
+             mat_params['tau'], 
+             mat_params['perm'], 
+             mat_params['top_trac'], 
+             mat_params['body_force'] )
+
+    ## ending time
+    time_end = time.time()
+
+    return time_end - time_start
+
+
+
